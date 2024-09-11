24cae38dedf3e558b539b22fe6ea84594931925b
Arif akib <a.rabbani.akib@gmail.com>
2024-09-11 06:03:26 +0600

    star issue fix

--- src/components/Shared/Star.jsx
+++ src/components/Shared/Star.jsx
@@ -1,17 +1,21 @@
-export default function SheredStar({content}) {
+import starfull from "/src/assets/images/icon/starfull.png"
+import starhalf from "/src/assets/images/icon/starhalf.png"
+import star from "/src/assets/images/icon/star.png"
+
+export default function SheredStar({ content }) {
   
     return (
       <>
          <div className="flex">
                             {[...Array(Math.floor(content.star))].map((star, index) => (
                               <div key={index}>
-                                <img src="/src/assets/images/icon/starfull.png" alt="" className="size-[15px]" />
+                                <img src={starfull} alt="" className="size-[15px]" />
                               </div>
                             ))}
-                            <p>{ 5-content.star ==0 ? '' : <img src="/src/assets/images/icon/starhalf.png" alt="" className="size-[15px]"/> }</p>
+                            <p>{ 5-content.star ==0 ? '' : <img src={starhalf} alt="" className="size-[15px]"/> }</p>
                             {[...Array(5-Math.ceil(content.star))].map((star, index) => (
                               <div key={index}>
-                              <img src="/src/assets/images/icon/star.png" alt="" className="size-[15px]"/>
+                              <img src={star} alt="" className="size-[15px]"/>
                               </div>
                             ))}
                           </div>
